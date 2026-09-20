732202707713c23261d0fd6bbe88951b6fbb0aef
yazz1482 <yashm1482@gmail.com>
2026-09-20 22:37:43 +0530

    Rearranged Operators, Added more operators for 2 missing slots

--- ObjectModePies.py
+++ ObjectModePies.py
@@ -1,27 +1,21 @@
-#         # # Bottom Left
-#         # pie.operator( "object.hide_view_clear", icon="REC", text="Unhide All"
-#         # ).select=False
-#         # # Bottom Right
-#         # pie.operator( "object.hide_view_set", icon="REC", text="Solo Object"
-#         # ).unselected = True
-#
+from . import  CommonMenus
 
 ADD_MENU = [
     {
-        "slot": 5,
+        "slot": 7,
         "label": "Add Plane",
         "operator": "mesh.primitive_plane_add",
         "invoke": True,
     },
     {
-        "slot": 1,
+        "slot": 2,
         "label": "Add Cube",
         "operator": "mesh.primitive_cube_add",
         "invoke": True,
     },
     {
-        "slot": 3,
-        "label": "Search \nAdd Menu",
+        "slot": 4,
+        "label": "Search Add Menu",
         "operator": "wm.search_single_menu",
         "invoke": True,
         "props": {
@@ -38,15 +32,27 @@ ADD_MENU = [
         },
     },
     {
-        "slot": 4,
+        "slot": 1,
         "label": "Add Cylinder",
         "operator": "mesh.primitive_cylinder_add",
         "invoke": True,
     },
     {
-        "slot": 2,
+        "slot": 3,
         "label": "Add UVSphere",
         "operator": "mesh.primitive_uv_sphere_add",
+        "invoke": True,
+    },
+    {
+        "slot": 5,
+        "label": "Add Cone",
+        "operator": "mesh.primitive_cone_add",
+        "invoke": True,
+    },
+    {
+        "slot": 6,
+        "label": "Add Grid",
+        "operator": "mesh.primitive_grid_add",
         "invoke": True,
     },
 ]
@@ -54,7 +60,7 @@ ADD_MENU = [
 
 APPLY_TRANSFORMS_MENU = [
     {
-        "slot": 0,
+        "slot": 2,
         "label": "Rotation",
         "operator": "object.transform_apply",
         "invoke": True,
@@ -65,8 +71,8 @@ APPLY_TRANSFORMS_MENU = [
         },
     },
     {
-        "slot": 3,
-        "label": "All\nTransforms",
+        "slot": 4,
+        "label": "All Transforms",
         "operator": "object.transform_apply",
         "invoke": True,
         "props": {
@@ -76,8 +82,8 @@ APPLY_TRANSFORMS_MENU = [
         },
     },
     {
-        "slot": 2,
-        "label": "Rotation &\n Scale",
+        "slot": 5,
+        "label": "Rotation & Scale",
         "operator": "object.transform_apply",
         "invoke": True,
         "props": {
@@ -87,8 +93,30 @@ APPLY_TRANSFORMS_MENU = [
         },
     },
     {
-        "slot": 4,
-        "label": "  Apply\nTransforms\n Menu",
+        "slot": 7,
+        "label": "Location & Scale",
+        "operator": "object.transform_apply",
+        "invoke": True,
+        "props": {
+            "location": True,
+            "rotation": False,
+            "scale": True,
+        },
+    },
+    {
+        "slot": 6,
+        "label": "Location & Rotation",
+        "operator": "object.transform_apply",
+        "invoke": True,
+        "props": {
+            "location": True,
+            "rotation": True,
+            "scale": False,
+        },
+    },
+    {
+        "slot": 0,
+        "label": "  Apply Transforms Menu",
         "operator": "wm.call_menu",
         "invoke": True,
         "props": {
@@ -96,7 +124,7 @@ APPLY_TRANSFORMS_MENU = [
         },
     },
     {
-        "slot": 5,
+        "slot": 1,
         "label": "Location",
         "operator": "object.transform_apply",
         "invoke": True,
@@ -107,7 +135,7 @@ APPLY_TRANSFORMS_MENU = [
         },
     },
     {
-        "slot": 1,
+        "slot": 3,
         "label": "Scale",
         "operator": "object.transform_apply",
         "invoke": True,
@@ -122,7 +150,7 @@ APPLY_TRANSFORMS_MENU = [
 
 MODIFIER_MENU = [
     {
-        "slot": 2,
+        "slot": 1,
         "label": "SubDivision",
         "operator": "object.modifier_add",
         "invoke": True,
@@ -131,7 +159,7 @@ MODIFIER_MENU = [
         },
     },
     {
-        "slot": 3,
+        "slot": 2,
         "label": "Mirror",
         "operator": "object.modifier_add",
         "invoke": True,
@@ -140,7 +168,7 @@ MODIFIER_MENU = [
         },
     },
     {
-        "slot": 0,
+        "slot": 4,
         "label": "Modifier Search",
         "operator": "wm.search_single_menu",
         "invoke": True,
@@ -149,7 +177,7 @@ MODIFIER_MENU = [
         },
     },
     {
-        "slot": 1,
+        "slot": 7,
         "label": "Multires",
         "operator": "object.modifier_add",
         "invoke": True,
@@ -158,7 +186,7 @@ MODIFIER_MENU = [
         },
     },
     {
-        "slot": 4,
+        "slot": 3,
         "label": "Solidify",
         "operator": "object.modifier_add",
         "invoke": True,
@@ -168,6 +196,24 @@ MODIFIER_MENU = [
     },
     {
         "slot": 5,
+        "label": "Displace",
+        "operator": "object.modifier_add",
+        "invoke": True,
+        "props": {
+            "type": "DISPLACE",
+        },
+    },
+    {
+        "slot": 6,
+        "label": "Shrinkwrap",
+        "operator": "object.modifier_add",
+        "invoke": True,
+        "props": {
+            "type": "SHRINKWRAP",
+        },
+    },
+    {
+        "slot": 0,
         "label": "Array",
         "operator": "object.modifier_add_node_group",
         "invoke": True,
@@ -185,7 +231,7 @@ MODIFIER_MENU = [
 
 SELECTION_MENU = [
     {
-        "slot": 4,
+        "slot": 5,
         "label": "Deselect All",
         "operator": "object.select_all",
         "invoke": True,
@@ -194,7 +240,7 @@ SELECTION_MENU = [
         },
     },
     {
-        "slot": 2,
+        "slot": 3,
         "label": "Select All",
         "operator": "object.select_all",
         "invoke": True,
@@ -203,8 +249,8 @@ SELECTION_MENU = [
         },
     },
     {
-        "slot": 3,
-        "label": "Invert\nSelection",
+        "slot": 4,
+        "label": "Invert Selection",
         "operator": "object.select_all",
         "invoke": True,
         "props": {
@@ -212,17 +258,8 @@ SELECTION_MENU = [
         },
     },
     {
-        "slot": 0,
-        "label": "Right Click\nMenu",
-        "operator": "wm.call_menu",
-        "invoke": True,
-        "props": {
-            "name": "VIEW3D_MT_object_context_menu",
-        },
-    },
-    {
-        "slot": 5,
-        "label": "Rename\nObject",
+        "slot": 2,
+        "label": "Rename Object",
         "operator": "wm.call_panel",
         "invoke": True,
         "props": {
@@ -230,76 +267,46 @@ SELECTION_MENU = [
         },
     },
     {
-        "slot": 1,
-        "label": "Move To\nCollection",
+        "slot": 6,
+        "label": "Move To Collection",
         "operator": "wm.call_menu",
         "invoke": True,
         "props": {
             "name": "OBJECT_MT_move_to_collection",
         },
     },
-]
-
-SHADING_MENU = [
     {
         "slot": 0,
-        "label": "Shading\nMenu",
-        "operator": "wm.call_panel",
+        "label": "Unhide All",
+        "operator": "object.hide_view_clear",
         "invoke": True,
         "props": {
-            "name": "VIEW3D_PT_shading",
-        },
-    },
-    {
-        "slot": 4,
-        "label": "Wireframe",
-        "operator": "cop.cshading",
-        "invoke": True,
-        "props": {
-            "SetShading": "WIREFRAME",
-        },
-    },
-    {
-        "slot": 2,
-        "label": "Solid",
-        "operator": "cop.cshading",
-        "invoke": True,
-        "props": {
-            "SetShading": "SOLID",
-        },
-    },
-    {
-        "slot": 3,
-        "label": "Switch\nLighting",
-        "operator": "cop.cshading",
-        "invoke": True,
-        "props": {
-            "SwitchLighting": True,
+            "select": False,
         },
     },
     {
         "slot": 1,
-        "label": "Material",
-        "operator": "cop.cshading",
+        "label": "Solo Object",
+        "operator": "object.hide_view_set",
         "invoke": True,
         "props": {
-            "SetShading": "MATERIAL",
+            "unselected": True,
         },
     },
     {
-        "slot": 5,
-        "label": "Rendered",
-        "operator": "cop.cshading",
+        "slot": 7,
+        "label": "Hide Selected",
+        "operator": "object.hide_view_set",
         "invoke": True,
         "props": {
-            "SetShading": "RENDERED",
+            "unselected": False,
         },
     },
 ]
 
 TOOL_SELECT_MENU = [
     {
-        "slot": 4,
+        "slot": 5,
         "label": "Cursor Tool",
         "operator": "wm.tool_set_by_id",
         "invoke": True,
@@ -308,7 +315,7 @@ TOOL_SELECT_MENU = [
         },
     },
     {
-        "slot": 2,
+        "slot": 3,
         "label": "Lasso Select",
         "operator": "wm.tool_set_by_id",
         "invoke": True,
@@ -326,7 +333,7 @@ TOOL_SELECT_MENU = [
         },
     },
     {
-        "slot": 3,
+        "slot": 4,
         "label": "Tweak Tool",
         "operator": "wm.tool_set_by_id",
         "invoke": True,
@@ -335,7 +342,7 @@ TOOL_SELECT_MENU = [
         },
     },
     {
-        "slot": 5,
+        "slot": 7,
         "label": "Move Tool",
         "operator": "wm.tool_set_by_id",
         "invoke": True,
@@ -352,121 +359,26 @@ TOOL_SELECT_MENU = [
             "name": "builtin.scale",
         },
     },
-]
-
-VIEW_MENU = [
-    {
-        "slot": 0,
-        "label": "Top",
-        "operator": "view3d.view_axis",
-        "invoke": True,
-        "props": {
-            "type": "TOP",
-        },
-    },
-    {
-        "slot": 1,
-        "label": "Front",
-        "operator": "view3d.view_axis",
-        "invoke": True,
-        "props": {
-            "type": "FRONT",
-        },
-    },
     {
         "slot": 2,
-        "label": "Right",
-        "operator": "view3d.view_axis",
+        "label": "Add Cube Tool",
+        "operator": "wm.tool_set_by_id",
         "invoke": True,
         "props": {
-            "type": "RIGHT",
+            "name": "builtin.primitive_cube_add",
         },
     },
     {
-        "slot": 3,
-        "label": "Bottom",
-        "operator": "view3d.view_axis",
+        "slot": 6,
+        "label": "Add Cylinder Tool",
+        "operator": "wm.tool_set_by_id",
         "invoke": True,
         "props": {
-            "type": "BOTTOM",
-        },
-    },
-    {
-        "slot": 4,
-        "label": "Left",
-        "operator": "view3d.view_axis",
-        "invoke": True,
-        "props": {
-            "type": "LEFT",
-        },
-    },
-    {
-        "slot": 5,
-        "label": "Back",
-        "operator": "view3d.view_axis",
-        "invoke": True,
-        "props": {
-            "type": "BACK",
+            "name": "builtin.primitive_cylinder_add",
         },
     },
 ]
 
-MODE_MENU = [
-    {
-        "slot": 3,
-        "label": "Object Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "OBJECT",
-        },
-    },
-    {
-        "slot": 0,
-        "label": "Texture Paint\n Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "TEXTURE_PAINT",
-        },
-    },
-    {
-        "slot": 2,
-        "label": "Edit Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "EDIT",
-        },
-    },
-    {
-        "slot": 4,
-        "label": "Sculpt Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "SCULPT",
-        },
-    },
-    {
-        "slot": 1,
-        "label": "Weight Paint \n Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "WEIGHT_PAINT",
-        },
-    },
-    {
-        "slot": 5,
-        "label": "Vertex Paint \n Mode",
-        "operator": "object.mode_set",
-        "invoke": True,
-        "props": {
-            "mode": "VERTEX_PAINT",
-        },
-    },
-]
 
 
 MENUS = {
@@ -474,19 +386,22 @@ MENUS = {
     "OBJECT.APPLY_TRANSFORMS": APPLY_TRANSFORMS_MENU,
     "OBJECT.MODIFIER": MODIFIER_MENU,
     "OBJECT.SELECTION": SELECTION_MENU,
-    "OBJECT.SHADING": SHADING_MENU,
-    "OBJECT.TOOL_SELECT":TOOL_SELECT_MENU,
-    "OBJECT.VIEW":VIEW_MENU,
-    "OBJECT.MODE":MODE_MENU,
+    "OBJECT.TOOL_SELECT": TOOL_SELECT_MENU,
+
+    # Common Menus
+    "OBJECT.SHADING": CommonMenus.SHADING_MENU,
+    "OBJECT.VIEW": CommonMenus.VIEW_MENU,
+    "OBJECT.MODE": CommonMenus.MODE_MENU,
 }
 
 MENU_NAMES = {
-    "OBJECT.ADD": "Add",
+    "OBJECT.ADD": "Add Object",
     "OBJECT.APPLY_TRANSFORMS": "Apply Transforms",
     "OBJECT.MODIFIER": "Add Modifier",
-    "OBJECT.SELECTION": "Selection",
-    "OBJECT.SHADING": "Shading",
+    "OBJECT.SELECTION": "Selection & Hide",
     "OBJECT.TOOL_SELECT": "Tool Select",
+
+    "OBJECT.SHADING": "Shading",
     "OBJECT.VIEW": "View",
     "OBJECT.MODE": "Mode",
 }
